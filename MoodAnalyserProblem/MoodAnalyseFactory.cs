9427612db885b78bcde0148a3ef9f67b7668abeb
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-14 18:46:38 +0530

    [Mihir] Refactor MoodAnalyserFactory file to follow DRY rules

--- MoodAnalyserProblem/MoodAnalyseFactory.cs
+++ MoodAnalyserProblem/MoodAnalyseFactory.cs
@@ -1,35 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 
 namespace MoodAnalyserProblem
 {
     public class MoodAnalyseFactory
-    {
+    { 
+
+        public string Message { get; set; }
+
         public static object CreateMoodAnalyse(string className, string constructorName)
         {
-            string pattern = @"." + constructorName + "$";
-            Match result = Regex.Match(className, pattern);
-            if (result.Success)
-            {
-                try
-                {
-                    Assembly executing = Assembly.GetExecutingAssembly();
-                    Type moodAnalyseType = executing.GetType(className);
-                    return Activator.CreateInstance(moodAnalyseType);
-                }
-                catch (ArgumentNullException)
-                {
-                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class Not Found");
-                }
-            }
-            else
-            {
-                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
-            }
+            string message = null;
+            return CreateMoodAnalyseUsingParameterizedConstructor(className,constructorName, message);
         }
 
         public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
@@ -47,7 +35,7 @@ namespace MoodAnalyserProblem
                     throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
             }
             else
-                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class Not found");
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class Not Found");
         }
     }
 }
