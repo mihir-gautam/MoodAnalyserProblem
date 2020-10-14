0c37b2f9382635607fc1df37d58ee89ebdfdea41
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-14 18:11:32 +0530

    [Mihir] Add use of reflection to create object with parameterized constructor

--- MoodAnalyserProblem/MoodAnalyseFactory.cs
+++ MoodAnalyserProblem/MoodAnalyseFactory.cs
@@ -31,5 +31,23 @@ namespace MoodAnalyserProblem
                 throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
             }
         }
+
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo info = type.GetConstructor(new[] { typeof(string) });
+                    object instance = info.Invoke(new object[] { (message) });
+                    return instance;
+                }
+                else
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
+            }
+            else
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class Not found");
+        }
     }
 }
