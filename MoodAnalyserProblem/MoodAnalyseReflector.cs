6c0585ed4db8b963d55fc649196245511de1894d
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-14 19:10:48 +0530

    [Mihir] Refactor Changed name from MoodAnalyseFactory to MoodAnalyseReflector and then invoked methods

---
+++ MoodAnalyserProblem/MoodAnalyseReflector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyseReflector
+    { 
+        public static object CreateMoodAnalyse(string className, string constructorName)
+        {
+            string message = null;
+            return CreateMoodAnalyseUsingParameterizedConstructor(className,constructorName, message);
+        }
+
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo info = type.GetConstructor(new[] { typeof(string) });
+                    object instance = info.Invoke(new object[] { (message) });
+                    return instance;
+                }
+                else
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
+            }
+            else
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class Not Found");
+        }
+        public static string InvokeAnalyseMood(string message, string methodName)
+        {
+            try
+            {
+                Type type = Type.GetType("MoodAnalyserApp.MoodAnalyser");
+                object moodAnalyserObject = CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyserApp.MoodAnalyser", "MoodAnalyser", message);
+                MethodInfo method = type.GetMethod(methodName);
+                object mood = method.Invoke(moodAnalyserObject, null);
+                return mood.ToString();
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Method is Not found");
+            }
+        }
+    }
+}
